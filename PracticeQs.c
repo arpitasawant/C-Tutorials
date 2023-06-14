8c3df2dd296316766796fcb9c5d0048d565bf191
Arpita <arpitasawant2002@gmail.com>
2023-06-14 13:34:29 +0530

    "Updated"

--- PracticeQs.c
+++ PracticeQs.c
@@ -1,11 +1,22 @@
-// 1.Calculate area of a square
+
 
 #include <stdio.h>
 
 int main()
 {
-    int side = 20;
-    int area = side * side;
-    printf("Area of square is: %d",area);
+    /* 1.Calculate area of a square(side is given)
+    float side;
+    scanf("%f",&side);
+    float area = side * side;
+    printf("Area of square is: %f",area);*/
+
+    /* 2.Calculate area of a square(radius is given)*/
+    float radius;
+    float pi=3.14;
+    printf("Enter radius: ");
+    scanf("%f",&radius);
+    float area = pi*radius * radius;
+    printf("Area of square is: %f",area);
+    
     return 0;
 }
