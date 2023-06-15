5254f3f8304af05c4ce0eda372f59e44847356ac
Arpita <arpitasawant2002@gmail.com>
2023-06-15 20:17:51 +0530

    "Qs"

--- PracticeQs.c
+++ PracticeQs.c
@@ -1,3 +1,5 @@
+
+
 #include <stdio.h>
 
 int main()
@@ -24,10 +26,30 @@ int main()
     scanf("%d",&a);
     printf("%d",a % 2 == 0);*/
 
-    /*4.Check if given character is digit or not*/
-    char c;
+    /*4.Check if given character is digit or not
+    char ch;
     printf("Enter Character: ");
-    scanf("%c",&c);
-    
+    scanf("%c",&ch);
+    // printf("ch is digit %c",ch>='0' && ch<='9');
+    if(ch>='0' && ch<='9')
+	 {
+	  	printf("%c is DIGIT.", ch);
+	 }
+	 else
+	 {
+	  	printf("%c is NOT DIGIT.", ch);
+	 }*/
+
+     /*5.print the smallest number*/
+     int a,b;
+     scanf("%d",&a);
+     scanf("%d",&b);
+     
+     if(a<b){
+        printf("Smallest number is: %d",a);
+     }
+     else{
+        printf("Smallest number is: %d",b);
+     }
     return 0;
 }
