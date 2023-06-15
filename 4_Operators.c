ff93a769177e4ccb4b98a86e45e2e44b84830b83
Arpita <arpitasawant2002@gmail.com>
2023-06-15 19:58:51 +0530

    "updated"

---
+++ 4_Operators.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/*operations percedence
+1. !
+2. * / %
+3. +-
+4. <,<=,>,>=
+5. ==,!=
+6. &&
+7. ||
+8. =
+*/
+
+int main()
+{
+    // Associativity(for same precedence)--> left to right
+    int a = 4 * 3 / 6 * 2;
+    printf("%d\n", a);
+
+    /*Relational Operators
+    ==
+    >,<,>=,<=
+    !=
+    True = 1,False = 0
+    */
+    printf("%d\n", 3>=3);
+
+    /*Logical Operators
+    && = AND
+    || = OR
+    ! = NOT
+    */
+
+    printf("%d", 3>4 && 4>=4);
+    printf("%d", 3>4 || 4>=4);
+    printf("%d", !(3>4 && 4>=4));
+
+    /*
+    Assignment Operators(shorthand operator)
+    = , += , -= , *= , /= , %=
+    */
+    return 0;
+}
--- PracticeQs.c
+++ PracticeQs.c
@@ -10,13 +10,26 @@ int main()
     float area = side * side;
     printf("Area of square is: %f",area);*/
 
-    /* 2.Calculate area of a square(radius is given)*/
+    /* 2.Calculate area of a square(radius is given)
     float radius;
     float pi=3.14;
     printf("Enter radius: ");
     scanf("%f",&radius);
     float area = pi*radius * radius;
-    printf("Area of square is: %f",area);
+    printf("Area of square is: %f",area);*/
+    
+    /*3.Check if a number is divisible by 2 or not
+    
+    
+    int a;
+    printf("Enter number: ");
+    scanf("%d",&a);
+    printf("%d",a % 2 == 0);*/
+
+    /*4.Check if given character is digit or not*/
+    char c;
+    printf("Enter Character: ");
+    scanf("%c",&c);
     
     return 0;
 }
