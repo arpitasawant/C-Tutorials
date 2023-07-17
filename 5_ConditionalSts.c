4c4eb742f1640b97b9b3677fcb2f3e015caa73a1
Arpita <arpitasawant2002@gmail.com>
2023-07-17 19:28:45 +0530

    Updated

--- 5_ConditionalSts.c
+++ 5_ConditionalSts.c
@@ -1,6 +1,26 @@
 #include <stdio.h>
 
+/*Conditional Statements
+1.if-else
+2.switch*/
 int main() {
-    printf("Hello World!");
+    
+    /*If-else
+    if(condition){
+       // do something if true
+    }
+    else{
+       // do something if False
+    }*/
+
+    int age;
+    printf("Enter Age: ");
+    scanf("%d",&age);
+    if(age>=18){
+        printf("You are an adult");
+    }
+    else{
+       printf("You are not an adult");
+    }
     return 0;
 }
