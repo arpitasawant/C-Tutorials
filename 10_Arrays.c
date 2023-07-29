570fdacedaf43e71357e8b123744c713f26f26da
Arpita <arpitasawant2002@gmail.com>
2023-07-30 00:42:39 +0530

    updated

--- 10_Arrays.c
+++ 10_Arrays.c
@@ -25,6 +25,6 @@ scanf("%d",&marks[1]);
 printf("enter maths: ");
 scanf("%d",&marks[2]);
 
-printf("phy=%d,chem=%d,maths=%d",marks[0],marks[1],marks[2]);
+printf("phy=%d,chem= %d,maths= %d",marks[0],marks[1],marks[2]);
 return 0;
 }
